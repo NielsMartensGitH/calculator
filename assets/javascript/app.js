a922b111d380ce22e38040cc1e3c4be29891d9da
Niels <niels_martens@live.nl>
2021-11-24 15:15:04 +0100

    calculator can now calculate

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,15 @@
 const display = document.getElementById('display')
 
 function buttonClick(number) {
-    if (display.innerHTML == "DISPLAY HERE") {
+    if (display.innerHTML == "0" && number != 0) {
         display.innerHTML = number;
+    } else if (number == 0 && display.innerHTML == "0") {
+        console.log("Cannot start with zero!")
     } else {
         display.innerHTML += number;
     }
 }
 
+function calculate() {
+    document.getElementById("result").innerHTML = eval(display.innerHTML)
+}
