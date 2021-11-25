be06c83ba9ac213f93f8b2a2e3b76405a76f82f9
Niels <niels_martens@live.nl>
2021-11-25 15:03:16 +0100

    made input fit display , made restriction on starting input like zero, +, / or *

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,32 @@
 const display = document.getElementById('display')
 
 function buttonClick(number) {
-    if (display.innerHTML == "0" && number != 0) {
+    if (display.innerHTML == "0" && number != 0 && number != "." && number != "/" && number != "*" && number != "+") {
         display.innerHTML = number;
     } else if (number == 0 && display.innerHTML == "0") {
-        console.log("Cannot start with zero!")
-    } else {
+        console.log("Cannot start with zero!");
+    } else if (number == "." && display.innerHTML == "0") {
+        console.log("Cannot start with point!");
+    } else if (number == "*" && display.innerHTML == "0") {
+        console.log("Cannot start with *");
+    } else if (number == "/" && display.innerHTML == "0") {
+        console.log("Cannot start with /")
+    } else if (number == "+" && display.innerHTML == "0") {
+        console.log("Cannot start with +")
+    } else if (display.innerHTML == "-" && number == "-") {
+        console.log("Can't use - two times!")
+    }
+    else {
         display.innerHTML += number;
     }
 }
 
 function calculate() {
     document.getElementById("result").innerHTML = eval(display.innerHTML)
+}
+
+
+function reset() {
+    document.getElementById("display").innerHTML = "0";
+    document.getElementById("result").innerHTML = "";
 }
